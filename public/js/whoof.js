a9a5625deb333196ca3687fbc99ccd2a88c0085d
talysh <taleh.aliyev@gmail.com>
2020-11-16 20:53:27 -0500

    developing serving the index html using handlebars

---
+++ public/js/whoof.js
@@ -0,0 +1,58 @@
+// When the page loads, grab and display all of our whoofs
+$.get("/api/all", (data) => {
+  if (data.length !== 0) {
+    for (let i = 0; i < data.length; i++) {
+      const row = $("<div>");
+      row.addClass("whoof");
+
+      row.append("<p>" + data[i].author + " whoofed.. </p>");
+      row.append("<p>" + data[i].body + "</p>");
+      row.append(
+        "<p>On " + new Date(data[i].created_at).toLocaleDateString() + "</p>"
+      );
+
+      $("#whoof-area").prepend(row);
+    }
+  }
+});
+
+// When user whoofs (clicks addBtn)
+$("#whoof-submit").on("click", (event) => {
+  event.preventDefault();
+
+  // Make a newwhoof object
+  const newwhoof = {
+    author: $("#author")
+      .val()
+      .trim(),
+    body: $("#whoof-box")
+      .val()
+      .trim(),
+    createdAt: new Date()
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", " "),
+  };
+
+  console.log(newwhoof);
+
+  // Send an AJAX POST-request with jQuery
+  $.post("/api/new", newwhoof)
+    // On success, run the following code
+    .then(() => {
+      const row = $("<div>");
+      row.addClass("whoof");
+
+      row.append("<p>" + newwhoof.author + " whoofed: </p>");
+      row.append("<p>" + newwhoof.body + "</p>");
+      row.append(
+        "<p>On " + new Date(newwhoof.createdAt).toLocaleDateString() + "</p>"
+      );
+
+      $("#whoof-area").prepend(row);
+    });
+
+  // Empty each input box by replacing the value with an empty string
+  $("#author").val("");
+  $("#whoof-box").val("");
+});
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 
+const exphbs = require("express-handlebars");
+
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
@@ -19,7 +21,10 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+// Handlebars
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
